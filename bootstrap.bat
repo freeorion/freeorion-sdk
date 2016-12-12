1ccad9a9949d81ed78b0c8f3b339130b0579ed75
Marcel Metz <mmetz@adrian-broher.net>
2016-12-12 18:19:16 +0100

    Update bootstrap.bat output
    Adjusted and reworded that Visual Studio 2015 or later is required now to use
    the Windows SDK.

--- bootstrap.bat
+++ bootstrap.bat
@@ -4,7 +4,7 @@ echo          FreeOrion Windows SDK bootstrapper
 echo ----------------------------------------------------
 echo For help on compiling FreeOrion have a look at the Wiki:
 echo http://freeorion.org/index.php/Compile_In_Windows
-echo Note: You need to have MSVC 2013 installed to use the SDK
+echo Note: You need to have MSVC 2015 installed to use the SDK
 echo ----------------------------------------------------
 echo Cloning the FreeOrion repository from GitHub...
 cd "%~d0%~p0"
@@ -15,7 +15,7 @@ echo Done.
 echo ----------------------------------------------------
 echo To update your clone of the repo later use your git client
 echo The MSVC solution is located at:
-echo %~d0%~p0FreeOrion\msvc2013\FreeOrion.sln
+echo %~d0%~p0FreeOrion\msvc2015\FreeOrion.sln
 echo now. Have fun while developing! :D
 pause
 del bootstrap.bat

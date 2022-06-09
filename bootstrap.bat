3baef8b8c46083245c3e31ae1d6f974b08bb86db
Geoff <geoffthemedio@users.noreply.github.com>
2022-06-09 10:08:53 +0200

    Update bootstrap.bat
    - refer to BUILD.md instead of the wiki
    -refer to 2019/2022 instead of 2017/2019 project files

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,10 +2,10 @@
 echo ----------------------------------------------------
 echo          FreeOrion Windows SDK bootstrapper
 echo ----------------------------------------------------
-echo For help on compiling FreeOrion have a look at the Wiki:
-echo http://freeorion.org/index.php/Compile_In_Windows
-echo Note: You need to have MSVC 2017 or 2019 installed to use
-echo the SDK
+echo For help on compiling FreeOrion, look at at BUILD.md in
+echo the FreeOrion repository root directory.
+echo Note: You need to have MSVC 2019 or 2022 installed to use
+echo the SDK to build FreeOrion.
 echo ----------------------------------------------------
 echo Cloning the FreeOrion repository from GitHub...
 cd "%~d0%~p0"
@@ -15,8 +15,8 @@ rmdir bin
 echo Done.
 echo ----------------------------------------------------
 echo To update your clone of the repo later use your git client
-echo The MSVC 2017 solution is located at:
-echo %~d0%~p0FreeOrion\msvc2017\FreeOrion.sln
+echo The MSVC 2019 solution is located at:
+echo %~d0%~p0FreeOrion\msvc2019\FreeOrion.sln
 echo now. Have fun while developing! :D
 pause
 del bootstrap.bat

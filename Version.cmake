dd8b9b37d056659932ed18ce7dbcaa9f40e3c730
O01eg <o01eg@yandex.ru>
2023-04-25 22:11:51 +0300

    Update ChangeLog and Version for v14 release

--- Version.cmake
+++ Version.cmake
@@ -1,4 +1,4 @@
 # This file contains CMake variable definition of SDK version
 
-set(DIST_VERSION 13 CACHE STRING "FreeOrionSDK release version.")
+set(DIST_VERSION 14 CACHE STRING "FreeOrionSDK release version.")
 

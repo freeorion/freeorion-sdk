b49cbab46f631fb0d5151ab67c64b05e4d8ad960
O01eg <o01eg@yandex.ru>
2024-12-22 21:21:39 +0400

    Update SDL to 2.24.2

--- Sources.cmake
+++ Sources.cmake
@@ -34,9 +34,9 @@ set(FREETYPE_VERSION 2.9)
 set(FREETYPE_URL http://downloads.sourceforge.net/project/freetype/freetype2/${FREETYPE_VERSION}/freetype-${FREETYPE_VERSION}.tar.gz)
 set(FREETYPE_MD5 d23a1b246e78890458c6672cb45eacd0)
 
-set(SDL_VERSION 2.24.0)
+set(SDL_VERSION 2.24.2)
 set(SDL_URL https://www.libsdl.org/release/SDL2-${SDL_VERSION}.tar.gz)
-set(SDL_MD5 cf539ffe9e0dd6f943ac9de75fd2e56e)
+set(SDL_MD5 84c71cb2a14aa0d9504513c0b9fcb17c)
 
 set(LIBOGG_VERSION 1.3.2)
 set(LIBOGG_URL http://downloads.xiph.org/releases/ogg/libogg-${LIBOGG_VERSION}.tar.gz)
